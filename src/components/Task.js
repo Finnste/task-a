2dbc0b8a115220373e789db45fdcc4db8c1326c5
Finnste <Finn.Steffens@gmx.de>
2023-10-25 10:27:58 +0100

    add edit functionality

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,41 @@ import "../styles/Task.css";
 
 function Task({ tasks, completeTask, deleteTask }) {
   const [status, setStatus] = useState("task completed");
+  const [editable, setEditable] = useState(false);
+
+  const endEdit = (e) => {
+    console.log(e.which)
+    if (e.which == 10 || e.which == 13) {
+      setEditable(false);
+    }
+  }
 
   return tasks.map((task, index) => (
     <li key={index} className={task.isComplete ? "task complete" : "task"}>
-      <div>{task.name}</div>
-      <input className="checkbox" type="checkbox" onClick={() => completeTask(task.id)}/>
-      <button className="deleteButton" key={task.id} onClick={() => deleteTask(task.id)}>
+      <input
+        className={editable ? "taskTitle taskEdit": "taskTitle"}
+        defaultValue={task.name}
+        disabled={!editable}
+        onKeyDown={endEdit}
+      ></input>
+      <input
+        className="checkbox"
+        type="checkbox"
+        onClick={() => completeTask(task.id)}
+      />
+      <button
+        className="deleteButton"
+        key={task.id}
+        onClick={() => deleteTask(task.id)}
+      >
         delete
+      </button>
+
+      <button
+        className="deleteButton"
+        onClick={() => setEditable(!editable)}
+      >
+        edit
       </button>
     </li>
   ));
